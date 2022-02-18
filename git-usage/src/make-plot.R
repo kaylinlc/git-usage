3eb4dac2f825b030d1c7809acf909bd9ce068043
kaylinlc <99960840+kaylinlc@users.noreply.github.com>
2022-02-18 11:17:46 +0000

    Update make-plot.R

--- git-usage/src/make-plot.R
+++ git-usage/src/make-plot.R
@@ -6,22 +6,17 @@ g <- ggplot(
   data = sods_data,
   mapping = aes(x = year, y = percentage)) +
   geom_point() +
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm", colour = "darkgrey") +
   geom_text(
     aes(x = 2020, y = 82.8, label = "only GitHub"),
+    size = 3,
     nudge_x = 0.2,
-    nudge_y = -4) +
+    nudge_y = -6) +
   labs(
     x = "Year",
-    y = "Percentage who used git",
-    title = "Git usage has increased",
-    subtitle = "Data from Stackoverflow Developer Survey")
-
-ggsave(filename = "out/git-usage.png",
-       plot = g,
-       height = 7.4,
-       width = 10.5,
-       units = "cm")
+    y = "Percentage who used git") +
+  ylim(c(0,100)) +
+  theme_bw()
 
 sink(file = "out/regression-summary.txt")
 summary(lm(percentage ~ year, data = sods_data))
